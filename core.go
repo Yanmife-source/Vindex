f76478e1f917d1763ad10309b8be750737c630f4
Alista Tudor <oluwayanmifenokunlola@gmail.com>
2026-09-22 14:41:19 +0100

    Broke down the code into smaller,more manageble functions

--- core.go
+++ core.go
@@ -12,38 +12,47 @@ func main(){
 		return
 	}
 	url:=os.Args[1]
-	resp,err:=http.Get(url)
-	if err!=nil{
-		fmt.Println("Could not reach target:", url)
-    	fmt.Println("(is the server running? try again in a few seconds)")
-		//fmt.Println("Error fetching URL:", err)
+	resp,err:=fetchURL(url)
+	if err!=nil {
+		fmt.Print("Error: ",err)
 		return
 	}
 	defer resp.Body.Close()
 
-	//To print the headers found and the status code of the website at the url
-	fmt.Println("Status code:", resp.Status)
-	fmt.Println("Headers: ")
-	for key,values := range resp.Header {
-		for _,value:=range values {
-			fmt.Printf("%s: %s\n",key,value)
-		}
+	check_headers(resp)
 
-	}
+	// //To print the headers found and the status code of the website at the url
+	// fmt.Println("Status code:", resp.Status)
+	// fmt.Println("Headers: ")
+	// for key,values := range resp.Header {
+	// 	for _,value:=range values {
+	// 		fmt.Printf("%s: %s\n",key,value)
+	// 	}
+
+	// }
 
 	// Checks the  headers if the improtant secrutiy  headers are present or permissive 
-	required_headers:=[]string{"Content-Security-Policy", "X-Frame-Options", "Strict-Transport-Security"}
-	missing_headers:=[]string{}
-	for _,header:=range required_headers {
-		if resp.Header.Get(header)==""{
-			fmt.Println("[MISSING]", header)
-			missing_headers=append(missing_headers,header)
-		} else {
-			fmt.Println("[OK]", header, "-", resp.Header.Get(header))
-		
-		}
-	}
 	
-
 }
 
+func fetchURL(url string) (*http.Response,error) {
+	resp, err := http.Get(url)
+    if err != nil {
+        return nil, fmt.Errorf("could not reach %s: %w", url, err)
+    }
+    return resp, nil
+}
+
+// Checks the  headers if the important secrutiy  headers are present or permissive 
+func check_headers(resp *http.Response) {
+	required_headers:=[]string{"Content-Security-Policy", "X-Frame-Options", "Strict-Transport-Security"}
+	for _,header:=range required_headers {
+		fmt.Println("== Important Security headers ==")
+		if resp.Header.Get(header)==""{
+			fmt.Println("[MISSING]", header)
+		} else {
+			fmt.Println("[OK]", header, "-", resp.Header.Get(header))
+		}
+	}
+
+}
